de31bd8ab65964a0aab30d31b138805733c6c9ef
Justin <jpham79@hawaii.edu>
2019-09-14 21:39:06 -1000

    T-14:  Created routes for getting manga (get + list) with pagination

---
+++ services/web/backend/routes/paths/manga.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const manga = require('../../controllers/manga');
+const pagination = require('../middleware/pagination');
+
+module.exports = app => {
+    app.route('/api/manga')
+        .get(pagination.paginate, manga.criteria, manga.list);
+
+    app.route('/api/manga/:name')
+        .get(manga.get)
+}
