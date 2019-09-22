f7991145f95188a3a6cd2e72162d05cb89fab1a5
Justin <jpham79@hawaii.edu>
2019-09-22 00:42:50 -1000

    Beginning to optimize requests and insertion

--- services/crawler/bot.py
+++ services/crawler/bot.py
@@ -26,8 +26,13 @@ db = pymongo.MongoClient().mangabois
 async def fetch(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            print('awaiting something')
             return await response.text()
+
+async def parseXML():
+    bulkChapters = []
+    chapter_nums = []
+    manga = {}
+    prevNum = -1
 
 """
  Will only be checking for sitemaps,
@@ -54,10 +59,9 @@ async def crawl():
             xmlFile = await fetch(xml)
             parsedXml = BeautifulSoup(xmlFile, 'xml')
             # mangaLinks.append(link)
-            bulkChapters = []
-            chapterNums = []
-            manga = {}
-            prevNum = -1
+
+            curr_manga = None
+            chapters = []
             for location in parsedXml.find_all('loc'):
                 link = location.contents[0]
                 # print(link)
@@ -65,75 +69,20 @@ async def crawl():
                 if link.endswith('.xml'):
                     siteMaps.append(link)
                 else:
-  
-  
+                    if curr_manga is None and 'chapter' not in link:
+                        curr_manga = link
                     if 'chapter' in link:
-                        mangaChapterLinks.append(link)
                         # print(link)
-                        # fuck this manga tho, so i skipped it. it's like hella chapters long
-                        if 'yume_maboroshi' not in link:
-                            mangaName = link.rsplit('/')[4]
-                            mangaName = mangaName.replace('_', ' ')
-                            m = re.search("chapter_(.*)", link)
-                            currNum = float(m.group(1))
-                            # print(currNum)
-                            # print(prevNum)
+                        chapters.append(link)
+                    elif 'chapter' not in link and curr_manga is not None and link != curr_manga:
+                        # print(curr_manga, chapters)
+                        await parse(curr_manga, chapters)
+                        chapters.clear()
+                        curr_manga = link
 
-                            # This checks if we have moved on to another manga
-                            # this was done before i started grabbing manga name
-                            # pages = await getPages(link)
-                            if currNum > prevNum:
-                                pages = await getPages(link)
-                                chapter = {'num': currNum, 'pages': pages, 'manga': {'name': mangaName}}
-                                bulkChapters.append(chapter)
-                                chapterNums.append(currNum)
-                                prevNum = currNum
-                                # getPages(link)
-                                # print(chapter)
-                            else:
-                                pages = await getPages(link)
-                                # Once we have all the chapters for a manga we go ahead and batch insert them into the db
-                                chapters = []
-
-                                # If the insert succeeds, it returns a list of objectids we can use to reference the chapters from
-                                # chapterIds = db.chapters.insert_many(bulkChapters).inserted_ids
-                                chapterIds = []
-                                for chapter in bulkChapters:
-                                    count = db.chapters.count_documents({'manga': {'name': chapter['manga']['name']}, 'num': chapter['num']})
-                                    if count is 0:
-                                        chapterId = db.chapters.insert_one(chapter).inserted_id
-                                        chapterIds.append(chapterId)
-
-                                # Creating the chapter object to attach to the manga object which we will later insert
-                                for i in range(0, len(chapterIds)):
-                                    formattedChapter = {'num': chapterNums[i], 'chapterId': chapterIds[i]}
-                                    chapters.append(formattedChapter)
-                                manga['chapters'] = chapters
-
-                                # clearing the slate and starting over for the next manga
-                                bulkChapters.clear()
-                                chapterNums.clear()
-                                prevNum = -1
-                                # don't forget to append the manga chapter that failed the condition to a new array
-                                chapter = {'num': currNum, 'pages': pages, 'manga': {'name': mangaName}}
-                                bulkChapters.append(chapter)
-                                chapterNums.append(currNum)
-                    else:
-                        # If the chapter is not in the link, we know it is the landing page, so we grab metadata here
-                        testlink = await get_manga_info(link)
-                        if testlink is not None:
-                            # mangaLinks.append(get_manga_info(link))
-                            if 'yume_maboroshi' not in link:
-                                manga = testlink
-
-                    # If the chapters have been inserted into the database
-                    # We will insert the manga here with the reference to the ids
-                    if 'chapters' in manga:
-                        count = db.mangas.count_documents({'name': manga['name']})
-                        if count is 0:
-                            db.mangas.insert_one(manga)
-                            print(f"Just inserted: {manga['name']}")
-                        manga = {}
+                        
+  
+                    
     
             if requestRate:
                time.sleep(requestRate) 
@@ -217,12 +166,107 @@ async def get_manga_info(info_url):
         
         return data
 
-# if __name__ == "__main__":
-#     # asyncio.run(crawl())
-#     # asyncio.ensure_future(crawl())
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(main())
-#     await crawl()
+async def insertChapters(chapter):
+
+    chapter_ids = []
+    count = db.chapters.count_documents({'manga': {'name': chapter['manga']['name']}, 'num': chapter['num']})
+    if count is 0:
+        chapter_id = db.chapters.insert_one(chapter).inserted_id
+        chapter_ids.append(chapter_id)
+
+    return chapter_ids
+
+async def insertManga(manga):
+    pass
+
+async def parse(currChapter, chapters):
+    # print(currChapter, chapters)
+    bulkChapters = []
+    chapter_nums = []
+    manga = {}
+    manga_name = currChapter.rsplit('/')[4]
+    manga_name = manga_name.replace('_', ' ')
+
+    tasks = []
+    for link in chapters:
+        task = asyncio.create_task(getPages(link))
+        tasks.append(task)
+    result_chapters = await asyncio.wait(tasks)
+
+    for link in chapters:
+        m = re.search("chapter_(.*)", link)
+        currNum = float(m.group(1))
+        chapter_nums.append(currNum)
+
+    chapter_tasks = []
+    for chapter in result_chapters: 
+        for index, pages in enumerate(chapter):  # pages are sets of pages for a chapter
+            chapter = {'num': chapter_nums[index], 'pages': pages.result(), 'manga': {'name': manga_name}}
+            task = asyncio.create_task(insertChapters(chapter))
+            chapter_tasks.append(task)
+    
+    result_chapter_insert = await asyncio.wait(chapter_tasks)
+    for chapter_ids in result_chapter_insert:
+        for chapter_id in chapter_ids:
+            print(chapter_id.result())
+
+
+    # print(count)
+    # print(f"length of chapters: {len(result_chapters)}")
+    # print(f"length of chapter_nums: {len(chapter_nums)}")
+
+    
+    # 
+    # bulkChapters.append(chapter)
+    # chapter_nums.append(currNum)
+
+    #     # getPages(link)
+    #     # print(chapter)
+
+
+    #     # Once we have all the chapters for a manga we go ahead and batch insert them into the db
+    #     chapters = []
+
+    #     # If the insert succeeds, it returns a list of objectids we can use to reference the chapters from
+    #     # chapterIds = db.chapters.insert_many(bulkChapters).inserted_ids
+    #     chapterIds = []
+    #     for chapter in bulkChapters:
+    #         count = db.chapters.count_documents({'manga': {'name': chapter['manga']['name']}, 'num': chapter['num']})
+    #         if count is 0:
+    #             chapterId = db.chapters.insert_one(chapter).inserted_id
+    #             chapterIds.append(chapterId)
+
+    #     # Creating the chapter object to attach to the manga object which we will later insert
+    #     for i in range(0, len(chapterIds)):
+    #         formattedChapter = {'num': chapter_nums[i], 'chapterId': chapterIds[i]}
+    #         chapters.append(formattedChapter)
+    #     manga['chapters'] = chapters
+
+    #     # clearing the slate and starting over for the next manga
+    #     bulkChapters.clear()
+    #     chapter_nums.clear()
+
+    #     # don't forget to append the manga chapter that failed the condition to a new array
+    #     chapter = {'num': currNum, 'pages': pages, 'manga': {'name': manga_name}}
+    #     bulkChapters.append(chapter)
+    #     chapter_nums.append(currNum)
+    # else:
+    #     # If the chapter is not in the link, we know it is the landing page, so we grab metadata here
+    #     testlink = await get_manga_info(link)
+    #     if testlink is not None:
+    #         # mangaLinks.append(get_manga_info(link))
+    #         if 'yume_maboroshi' not in link:
+    #             manga = testlink
+
+    # # If the chapters have been inserted into the database
+    # # We will insert the manga here with the reference to the ids
+    # if 'chapters' in manga:
+    #     count = db.mangas.count_documents({'name': manga['name']})
+    #     if count is 0:
+    #         db.mangas.insert_one(manga)
+    #         print(f"Just inserted: {manga['name']}")
+    #     manga = {}
+
 asyncio.run(crawl())
 
 # y = json.dumps({'manga': mangaLinks}, indent=3)
