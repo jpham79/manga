cd76b90853592430ef4520eedb7213c5da4c704d
SoraZodia <sorazodia@gmail.com>
2019-09-22 22:42:39 -1000

    link the new entities to home

--- services/web/frontend/src/view/landing/home/Home.jsx
+++ services/web/frontend/src/view/landing/home/Home.jsx
@@ -6,55 +6,35 @@ import { Grid, Image } from 'semantic-ui-react';
 export class Home extends React.Component {
 
     render() {
+        let { mangas } = this.props;
+
+        let columnMapper = (index) => {
+            let columns = [];
+            for (let x = index; x < index + 3 && index < mangas.length; x++) {
+                columns.push(
+                    <Grid.Column width={3}>
+                        <Image size='large' src={mangas[x].image} />
+                    </Grid.Column>
+                );
+            }
+
+            return columns;
+        }
+        
         return (
             <Grid centered stackable columns={3}>
-                <Grid.Row>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                </Grid.Row>
-
-                <Grid.Row>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                </Grid.Row>
-
-                <Grid.Row>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                </Grid.Row>
-
-                <Grid.Row>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <Image size='large' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                    </Grid.Column>
-                </Grid.Row>
+                { mangas.map((manga, index) => {
+                    if (index % 3 === 0) {
+                        return (
+                            <Grid.Row>
+                                { columnMapper(index) }
+                            </Grid.Row>
+                        )
+                    }
+                    else {
+                        return '';
+                    }
+                }) }
             </Grid>
         )
     }
