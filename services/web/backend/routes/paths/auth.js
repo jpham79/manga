65d46b7bdffb07d32aab6d38ded10a1239cb58d7
MaxHirata <max.hirata118@gmail.com>
2019-09-23 01:02:56 -0700

    Test Commit with comment

--- services/web/backend/routes/paths/auth.js
+++ services/web/backend/routes/paths/auth.js
@@ -8,6 +8,8 @@ module.exports = app => {
         .get(authMiddleware.auth, authController.get)
         .post(authController.post);
 
+
+    // maybe remove this
     app.route('/api/auth/controller')
         .get((req, res) => {
             res.send(testController.testAuth());
