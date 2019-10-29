3acf436f789c74b71511c2d4a78401492c64ab82
SoraZodia <sorazodia@gmail.com>
2019-10-28 18:06:44 -1000

    Change header text and add view all link

--- services/web/frontend/src/view/landing/home/Home.jsx
+++ services/web/frontend/src/view/landing/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
+import { Link } from "react-router-dom"
 
 import { Grid, Image, Segment, Label } from 'semantic-ui-react';
 import MangaList from '../../../components/mangaList/MangaList.jsx';
 import '../landing.scss';
+import { ROUTES } from '../../../root/App.js'
+import '../../../root/App.scss'
 
 export class Home extends React.Component {
     
@@ -12,10 +15,16 @@ export class Home extends React.Component {
         
         return (
             <div className="home">
-                List 1:
+                <div className="heading-content">
+                    <span className="sub-heading">Hot Updates:</span>
+                    <span className="right-end"><Link to={ROUTES.favorites}>View All</Link></span>
+                </div>
                 <MangaList mangas={mangas} selectManga={selectManga} />
 
-                List 2:
+                <div className="heading-content">
+                    <span className="sub-heading">{ isLoggedIn ? 'Recommended Mangas:' : 'Recently Updated:' }</span>
+                    <span className="right-end"><Link to={ROUTES.favorites}>View All</Link></span>
+                </div>
                 <MangaList mangas={mangas} selectManga={selectManga} />
             </div>
         )
