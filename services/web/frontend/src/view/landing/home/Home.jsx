06aae1fa0b2b9d6337d789d32f17ef36f59698b0
SoraZodia <sorazodia@gmail.com>
2019-10-29 17:50:19 -1000

    Add third list, change scroll-bar color

--- services/web/frontend/src/view/landing/home/Home.jsx
+++ services/web/frontend/src/view/landing/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Link } from "react-router-dom"
 
-import { Grid, Image, Segment, Label } from 'semantic-ui-react';
+import { Header, Segment, Icon } from 'semantic-ui-react';
 import MangaList from '../../../components/mangaList/MangaList.jsx';
 import '../landing.scss';
 import { ROUTES } from '../../../root/App.js'
@@ -10,6 +10,35 @@ import '../../../root/App.scss'
 
 export class Home extends React.Component {
     
+    getPersonizatedList({ mangas, selectManga, isLoggedIn }) {
+        let elements = [];
+        if (isLoggedIn) {
+            elements.push(
+                <div className="heading-content">
+                    <span className="sub-heading">Recommended Mangas:</span>
+                    <span className="right-end"><Link to={ROUTES.favorites}>View All</Link></span>
+                </div>
+            );
+            elements.push(
+                <MangaList mangas={mangas} selectManga={selectManga} />
+            );
+        }
+        else {
+            elements.push(
+                <div className="heading-content">
+                    <Segment placeholder inverted secondary>
+                        <Header icon>
+                        <Icon name='thumbs up outline' />
+                            Login to see your recommendation
+                        </Header>
+                    </Segment>
+                </div>
+            );
+        }
+
+        return elements;
+    }
+
     render() {
         let { mangas, selectManga, isLoggedIn } = this.props;
         
@@ -22,10 +51,13 @@ export class Home extends React.Component {
                 <MangaList mangas={mangas} selectManga={selectManga} />
 
                 <div className="heading-content">
-                    <span className="sub-heading">{ isLoggedIn ? 'Recommended Mangas:' : 'Recently Updated:' }</span>
+                    <span className="sub-heading">Recently Updated:</span>
                     <span className="right-end"><Link to={ROUTES.favorites}>View All</Link></span>
                 </div>
                 <MangaList mangas={mangas} selectManga={selectManga} />
+
+                {this.getPersonizatedList(this.props)}
+
             </div>
         )
     }
