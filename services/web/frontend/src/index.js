40984d1581b523b2488a3a39a78078554456cdef
Justin <jpham79@hawaii.edu>
2019-11-17 23:11:16 -1000

    Added sass support for materialize

--- services/web/frontend/src/index.js
+++ services/web/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/materialize-css/dist/css/materialize.css';
+import '../node_modules/materialize-css/sass/materialize.scss';
 import '../node_modules/materialize-css/dist/js/materialize.js';
 import App from './root/App';
 import * as serviceWorker from './serviceWorker';
