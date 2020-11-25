6e13720696d039e6330d7bfe9495d2dbb48e82aa
SoraZodia <SoraZodia@users.noreply.github.com>
2020-11-24 20:03:55 -1000

    Add ability to rearrange the pages before upload

--- services/web/frontend/src/view/upload/components/PageManager.jsx
+++ services/web/frontend/src/view/upload/components/PageManager.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 import '../styles/page-manager.scss';
 
 const PageManager = (props) => {
@@ -9,9 +11,28 @@ const PageManager = (props) => {
         const imgObjectList = [];
         if (files.length > 0) {
             for (let x = 0; x < files.length; x++)
-                imgObjectList.push(<img key={files[x].name} src={URL.createObjectURL(files[x])} width='300px'/>);
+                imgObjectList.push(
+                    <Draggable draggableId={files[x].name} key={files[x].name} index={x}>
+                        {
+                            (provided, snapshot) => (
+                                <img ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} src={URL.createObjectURL(files[x])} width='300px' index={x}/>
+                            )
+                        }
+                    </Draggable>
+                );
 
             updatePages(imgObjectList);
+        }
+    }
+
+    function onDragEnd(result) {
+        if (result.destination) {
+            const updatedPages = Array.from(pages);
+            const [displacedPage] = updatedPages.splice(result.destination.index, 1);
+
+            updatedPages.splice(result.source.index, 0, displacedPage);
+
+            updatePages(updatedPages);
         }
     }
 
@@ -22,7 +43,20 @@ const PageManager = (props) => {
                 <input type="file" onChange={(event) => processFiles(event.target.files)} multiple/> 
             </div>
             {
-                pages.map((img) => img)
+                pages.length > 0 ?
+                    <DragDropContext onDragEnd={onDragEnd}>
+                        <Droppable droppableId="drop-container" direction="horizontal">
+                            {
+                               (provided, snapshot) => (
+                                <div ref={provided.innerRef}>
+                                    {pages.map((page) => page)}
+                                    {provided.placeholder}
+                                </div>
+                               )
+                            }
+                        </Droppable>
+                    </DragDropContext> :
+                    <div></div>
             }
         </div>
     )
